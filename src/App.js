bdd1b31917a77266a551dc597d710ab3a9ff2e46
Dan <danhemerlein@gmail.com>
2019-08-02 20:24:33 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Products from './components/Products';
+import Cart from './components/Cart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isCartOpen: false,
+      checkout: { lineItems: [] },
+      products: [],
+      shop: {}
+    };
+
+    this.handleCartClose = this.handleCartClose.bind(this);
+    this.addVariantToCart = this.addVariantToCart.bind(this);
+    this.updateQuantityInCart = this.updateQuantityInCart.bind(this);
+    this.removeLineItemInCart = this.removeLineItemInCart.bind(this);
+  }
+
+  componentWillMount() {
+    this.props.client.checkout.create().then((res) => {
+      this.setState({
+        checkout: res,
+      });
+    });
+
+    this.props.client.product.fetchAll().then((res) => {
+      this.setState({
+        products: res,
+      });
+    });
+
+    this.props.client.shop.fetchInfo().then((res) => {
+      this.setState({
+        shop: res,
+      });
+    });
+  }
+
+  addVariantToCart(variantId, quantity) {
+    this.setState({
+      isCartOpen: true,
+    });
+
+    const lineItemsToAdd = [{ variantId, quantity: parseInt(quantity, 10) }]
+    const checkoutId = this.state.checkout.id
+
+    return this.props.client.checkout.addLineItems(checkoutId, lineItemsToAdd).then(res => {
+      this.setState({
+        checkout: res,
+      });
+    });
+  }
+
+  updateQuantityInCart(lineItemId, quantity) {
+    const checkoutId = this.state.checkout.id
+    const lineItemsToUpdate = [{ id: lineItemId, quantity: parseInt(quantity, 10) }]
+
+    return this.props.client.checkout.updateLineItems(checkoutId, lineItemsToUpdate).then(res => {
+      this.setState({
+        checkout: res,
+      });
+    });
+  }
+
+  removeLineItemInCart(lineItemId) {
+    const checkoutId = this.state.checkout.id
+
+    return this.props.client.checkout.removeLineItems(checkoutId, [lineItemId]).then(res => {
+      this.setState({
+        checkout: res,
+      });
+    });
+  }
+
+  handleCartClose() {
+    this.setState({
+      isCartOpen: false,
+    });
+  }
+
+  render() {
+    console.log(this.state)
+    console.log(this.state.products)
+    return (
+      <div className="App">
+        <header className="App__header">
+          {!this.state.isCartOpen &&
+            <div className="App__view-cart-wrapper">
+              <button className="App__view-cart" onClick={() => this.setState({ isCartOpen: true })}>Cart</button>
+            </div>
+          }
+          <div className="App__title">
+            <h1>{this.state.shop.name}</h1>
+          </div>
+        </header>
+        <Products
+          products={this.state.products}
+          client={this.props.client}
+          addVariantToCart={this.addVariantToCart}
+        />
+        <Cart
+          checkout={this.state.checkout}
+          isCartOpen={this.state.isCartOpen}
+          handleCartClose={this.handleCartClose}
+          updateQuantityInCart={this.updateQuantityInCart}
+          removeLineItemInCart={this.removeLineItemInCart}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import keys from './config';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Client from 'shopify-buy';
+
+const client = Client.buildClient({
+  storefrontAccessToken: keys.storefrontAccessToken,
+  domain: 'young-nauseous.myshopify.com'
+});
+
+ReactDOM.render(
+  <App client={client} />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
