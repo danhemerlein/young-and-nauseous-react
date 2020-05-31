77db9c57c18e1f07aedf6d2fa15507c3bbe1a617
Dan <danhemerlein@gmail.com>
2020-05-31 14:20:36 -0400

    add some contact info to the about page

--- src/components/Pages/AboutPage/index.js
+++ src/components/Pages/AboutPage/index.js
@@ -6,16 +6,29 @@ class AboutPage extends Component {
   render() {
     return (
       <div className="AboutPage flex flex-col items-center bg-pink w100 p2">
-
         <h2 className="HomePageHero__headline true-sketch-rg  aesthetic-font-modifier-outline-purple text-center py4">
           Young & Nauseous is a lifestyle and apparel brand based in Brooklyn,
           New York
         </h2>
 
-        <h2 className="HomePageHero__headline true-sketch-rg  aesthetic-font-modifier-outline-purple text-center py4">
-          Young & Nauseous was started by Dan Hemerlein from his bed while he was stoned and bored
-        </h2>
+        <h3 className="HomePageHero__headline true-sketch-rg  aesthetic-font-modifier-outline-purple text-center py4">
+          Young & Nauseous was started by Dan Hemerlein from his bed while he
+          was stoned and bored
+        </h3>
 
+        <p>Contact Information:</p>
+        <ul>
+          <li>
+            email:{" "}
+            <a href="mailto:youngandnauseous@gmail.com">
+              youngandnauseous@gmail.com
+            </a>
+          </li>
+          <li>
+            phone:{" "}
+            (914) 393 0687
+          </li>
+        </ul>
       </div>
     );
   }
